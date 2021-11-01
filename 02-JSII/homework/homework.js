14fc1a63857d0f26e3f38175ef79256829ab1d29
BTorena <bautistatorena@gmail.com>
2021-11-01 19:19:14 -0300

    JSII homework a la mitad

--- 02-JSII/homework/homework.js
+++ 02-JSII/homework/homework.js
@@ -4,12 +4,18 @@ function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
 
+    return array.pop();
 }
 
 function incrementarPorUno(array) {
   // "array" debe ser una arreglo de números enteros
   // Aumenta cada número por 1 y devuelve el array
   // Tu código:
+  for(var i = 0; i < array.length; i++){
+    array[i] = array[i] + 1;
+  }
+  return array;
+  
 
 }
 
@@ -17,6 +23,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 
 }
 
@@ -26,6 +34,13 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+   var frase = palabras[0];
+   for(var i = 1;i < palabras.length;i++){
+     frase = frase + ' ' + palabras[i];
+   }
+   return frase;
+  
+
 
 }
 
@@ -34,27 +49,45 @@ function arrayContiene(array, elemento) {
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
 
+  return array.includes(elemento);
+
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser un array de enteros
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-
+  var promedio = 0;
+  for (var i = 0;i < resultadosTest.length;i++){
+   promedio = promedio + resultadosTest[i];
+  }
+  return promedio / resultadosTest.length;
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser un array de enteros
   // Devuelve el número más grande
   // Tu código:
-
+  var nGrande = numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
+     if( numeros[i] > nGrande){
+       nGrande = numeros[i];
+     }
+  }
+  return nGrande;
 }
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+   if(arguments.length === 0) return 0;
 
+    var n = 1;
+    for (var i = 0; i < arguments.length; i++) {
+      n = n*arguments[i];
+      }
+    return n;
 }
 
 function cuentoElementos(arreglo){
@@ -82,7 +115,7 @@ function breakStatement(numero) {
   // Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   // Guardar cada nuevo valor en un array. 
   // Devolver el array
-  // Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
+  // Si en algún momento el valor de la promedio y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
   // devolver: "Se interrumpió la ejecución"
   // Pista: usá el statement 'break'
   // Tu código:
@@ -176,12 +209,12 @@ function agregarAmigo(usuario, nuevoAmigo) {
 
 }
 
-function sumarLikesDeUsuario(usuario) {
+function promediorLikesDeUsuario(usuario) {
   // "usuario" tiene una propiedad llamada "posts" que es un array
   // "posts" es un array de objetos "post"
   // Cada objeto "post" tiene una propiedad llamada "likes" que es un entero
-  // Suma todos los likes de todos los objetos "post"
-  // Devuelve la suma
+  // promedio todos los likes de todos los objetos "post"
+  // Devuelve la promedio
   // Tu código:
 
 }
@@ -213,5 +246,5 @@ module.exports = {
   tienePropiedad,
   actualizarPassword,
   agregarAmigo,
-  sumarLikesDeUsuario
+ promediorLikesDeUsuario
 };
